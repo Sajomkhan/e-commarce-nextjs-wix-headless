86b0916c97b468a67a060eab427db3a4e3eee099
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-07-02 22:33:04 +0600

    Searchbar & Cartmodal updated

---
+++ src/components/CartModal.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import Image from "next/image";
+
+const CartModal = () => {
+  const cartItems = true;
+
+  return (
+    <div className="w-max absolute top-12 right-0 flex flex-col gap-6 p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20 bg-white ">
+      {!cartItems ? (
+        <div>Cart is Empty</div>
+      ) : (
+        <>
+          <div className="flex flex-col gap-8">
+            {/* ================ Image and Image Text ================== */}
+            <div className="flex gap-4">
+              <Image
+                src="https://images.pexels.com/photos/20419416/pexels-photo-20419416/free-photo-of-camera-in-red-background.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                <div>
+                  <div className="flex items-center justify-between gap-8">
+                    <p className="font-semibold">Product Name</p>
+                    <div className="p-1 bg-gray-100 rounded-md">$49</div>
+                  </div>
+                  <div className="text-sm text-gray-500">available</div>
+                  <div className="flex justify-between text-sm mt-3">
+                    <span>Qty: 2</span>
+                    <button>Remove</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <Image
+                src="https://images.pexels.com/photos/20419416/pexels-photo-20419416/free-photo-of-camera-in-red-background.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                <div>
+                  <div className="flex items-center justify-between gap-8">
+                    <p className="font-semibold">Product Name</p>
+                    <div className="p-1 bg-gray-100 rounded-md">$49</div>
+                  </div>
+                  <div className="text-sm text-gray-500">available</div>
+                  <div className="flex justify-between text-sm mt-3">
+                    <span>Qty: 2</span>
+                    <button>Remove</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ================ Bottom ================== */}
+          <div>
+            <div className="flex items-center justify-between font-semibold">
+              <span>Subtotal</span>
+              <span>$49</span>
+            </div>
+            <p className="text-gray-500 text-sm mt-2 mb-4">
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+            <div className="flex justify-between text-sm">
+              <button className="rounded-md py-2 px-4 ring-1 ring-gray-300">View Cart</button>
+              <button className="rounded-md py-2 px-4 ring-1 ring-gray-300">Checkout</button>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CartModal;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,27 +1,40 @@
 import Link from "next/link";
 import Menu from "./Menu";
 import Image from "next/image";
+import SearchBar from "./SearchBar";
+import NavIcons from "./NavIcons";
 
 const Navbar = () => {
   return (
     <div className="h-[var(--nav-height)] relative">
-        {/*============== MOBILE============== */}
+      {/*============== MOBILE============== */}
       <div className="h-full flex items-center justify-between md:hidden">
         <Link href="/">
-        <div className="text-2xl tracking-wide font-semibold">AJOM</div>
+          <div className="text-2xl tracking-wide font-semibold">AJOM</div>
         </Link>
         <Menu />
       </div>
       {/* ============ BIGGER SCREEN ============ */}
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
-      {/* LEFT */}
-      <div className="w-1/3">
-      <Link href="/">
-      <Image src="/logo.png" alt="" width={60} height={60} />
-        <div className="text-2xl tracking-wide font-semibold">AJOM</div>
-        </Link></div>
-      {/* RIGHT */}
-      <div className="w-2/3"></div>
+        {/* LEFT */}
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-10">
+          <Link href="/" className="flex gap-2">
+            <Image src="/logo.png" alt="" width={35} height={35} />
+            <div className="text-2xl tracking-wide font-semibold">AJOM</div>
+          </Link>
+          <div className="hidden xl:flex gap-3">
+            <Link href="/">Home</Link>
+            <Link href="/shop">Shop</Link>
+            <Link href="/shop">Deals</Link>
+            <Link href="/shop">About</Link>
+            <Link href="/shop">Contact</Link>
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavIcons />
+        </div>
       </div>
     </div>
   );
